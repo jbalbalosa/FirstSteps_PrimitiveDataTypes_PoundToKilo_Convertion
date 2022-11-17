7eff7f51bacf635a5bee157bfa72b504fdbf2225
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-17 11:18:36 +0900

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass!");
+        System.out.println("Primitive Data Types");
+        System.out.println("Challege: Pound to Kilo Convertion");
+
+        double poundValue = 5;
+        double kiloValue = poundValue * .45359237;
+
+        System.out.println("Kilo: " + kiloValue);
+
     }
 }
